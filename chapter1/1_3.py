0767974576dfcbf4c73354558833fe23dffdb518
frank <frankyiyi@gmail.com>
2017-06-04 21:58:59 -0700

    1.4

--- chapter1/1_3.py
+++ chapter1/1_3.py
@@ -1,11 +1,31 @@
-# 1.3 URLify
+# 1.4 Palindrome Permutation:
+# i spend a lot of time trying to implement an algorithm for permutations but that is not needed
+# a better way to look at this is
+# check if each character has an even amout of characters
+# 1 odd character is allowed.  in the middle
 
-string = 'Mr John Smith '
+string ='Tact Coa'
 
-def urlify(s):
-    s = s.rstrip()
-    s = s.replace(' ', '%20')
-    return s
+def palindrome(s):
+    s = s.lower().replace(' ', '')
+    d = dict.fromkeys(s,False)
+    counter = 0
+    
+    for c in s:
+        if d[c] == False:
+            d[c] = True
+        else:
+            d[c] = False
 
-result = urlify(string)
+    for k in d:
+        if d[k] == True:
+            counter += 1
+
+    if counter > 1:
+        return False
+    else:
+        return True
+    
+
+result = palindrome(string)
 print(result)
