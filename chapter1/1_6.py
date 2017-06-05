4daa87dc3562489a2a5fa07ce604612516b7d6e1
frank <frankyiyi@gmail.com>
2017-06-05 11:36:18 -0700

    1.6 complete

---
+++ chapter1/1_6.py
@@ -0,0 +1,22 @@
+# 1.6 String Compression
+# a2b1c5a3
+
+string = 'aabcccccaaa'
+
+def compression(s):
+    compressed = ''
+    count = 0
+
+    for i in range(len(s)):
+        count += 1
+
+        if i >= len(s)-1 or s[i] != s[i+1]:
+            compressed += s[i]+str(count)
+            count = 0
+
+    return compressed
+    
+
+result = compression(string)
+print(result)
+
